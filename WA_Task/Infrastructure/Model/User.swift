9fc076cf0b8680200a725ef5133756e3bc44767d
ikakooo <i_chkhitunidze@cu.edu.ge>
2022-03-02 13:39:32 +0400

    project structure managing up

---
+++ WA_Task/Infrastructure/Model/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  WA_Task
+//
+//  Created by Irakli Chkhitunidzde on 02.03.22.
+//
+
+import Foundation
+
+struct User {
+    var username:String?
+    var password:String?
+}
