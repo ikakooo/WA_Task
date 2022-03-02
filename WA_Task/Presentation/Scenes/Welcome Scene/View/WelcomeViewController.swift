7492499ac7e91754e206b6f1fc88072a38c6aba0
ikakooo <i_chkhitunidze@cu.edu.ge>
2022-03-02 14:59:24 +0400

    welcome scene done

---
+++ WA_Task/Presentation/Scenes/Welcome Scene/View/WelcomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WelcomeViewController.swift
+//  WA_Task
+//
+//  Created by Irakli Chkhitunidzde on 02.03.22.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
